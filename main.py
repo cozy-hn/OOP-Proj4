9c256445523f5c13917095c95e57143761278cf4
hojooneum <bldolphin96@gmail.com>
2022-11-28 00:43:16 +0900

    feat: add basic models and services

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    # get user input in loop
+    # get betting money for user
+    # betting money for computer is 10000 won
+    # exit when user input is 0
+    print("This is the entry point")
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ models/Dealer.py
@@ -0,0 +1,3 @@
+# 패를 나눠주는 딜러입니다.
+class Dealer:
+    pass
