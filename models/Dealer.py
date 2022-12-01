59d3afe7c53835ec682edeb0ad0536ae54e17b64
Gangmin <goingtobeaman@naver.com>
2022-12-01 17:52:59 +0900

    edited rules for seot da

--- models/Dealer.py
+++ models/Dealer.py
@@ -1,3 +1,12 @@
 # 패를 나눠주는 딜러입니다.
+from Rule import rule 
+
 class Dealer:
-    pass
+    def Declare_winner():
+        return 0
+    def Distribute_Cards():
+        return 0
+    def Check_Game_Ended():
+        return 0
+    def Start_New_Round():
+        return 0
