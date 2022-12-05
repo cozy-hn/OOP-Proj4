0ab168d0e2065cfe15a9d3d130dff892f663831a
hojooneum <bldolphin96@gmail.com>
2022-12-05 23:57:34 +0900

    Update Dealer Class

--- models/Dealer.py
+++ models/Dealer.py
@@ -7,17 +7,14 @@ class Dealer:
     def __init__(self, deck: [Hand], table):
         self.deck: [Hand] = deck
         self.table = table
-    def announce_winner(self, players_in: [Player]) -> Player:
+    def announce_winner(self, player1: Player, player2: Player) -> Player:
         top_value: int = 0
-        top_player: Player = players_in[0]
-        for player in players_in:
-            value: int = self.table.getValue(player.hands)
-            if value > top_value:
-                top_value = value
-                top_player = player
-
-        return top_player
+        # 두 플레이어의 패를 비교해서 밸류가 높은 쪽이 승리합니다.
+        # 무승부는 존재하지 않습니다.
+        return player2
     def distribute_cards(self, player1: Player, player2: Player) -> None:
+        # 플레이어에게 카드를 분배합니다.
+        # 퍼블릭 메소드를 이용해서 플레이어 내부의 멤버변 수 hands에 새로운 패를 넣어줍니다.
         return
 
     def check_game_ended(self, players: [Player]) -> bool:
