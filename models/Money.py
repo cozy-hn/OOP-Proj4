78324abe0b80f5acc90da6a61296177d5aeb94ff
cozy-hn <110678456+cozy-hn@users.noreply.github.com>
2022-12-09 02:51:38 +0900

    Autoplayer money player 수정

--- models/Money.py
+++ models/Money.py
@@ -1,21 +1,17 @@
-# 베팅 금액을 책임지는 클래스입니다.
 class Money:
-    def __init__(self, amount: int = 0, currency: str = "won"):
-        #맴버변수 초기화
-        self.amount = amount
-        self.currency = currency
+    def __init__(self,money:int =0) -> None:
+        self.money=money
 
-    def set_amount(self : int):
-        print("배팅할 금액을 얼마로 설정하시겠습니까? ")
-        self.amount = input() 
-
-    def set_currency(self : str):
-        # 화폐 종류는 원으로 통일합시다!
-        print("화폐의 종류가 무엇인가요? ")
-        self.currency = input() 
-
-    def get_amount(self):
-        return self.amount
-
-    def get_currency(self):
-        return self.currency
+    def getmoney(self)->int:
+        return self.money    
+    
+    def setmoney(self,money)->None:
+        self.money=money
+        
+    def plus(self,money:int)->int:
+        self.money+=money
+        return self.money
+    
+    def minus(self,money:int)->int: #판에 깔린돈 객체 용
+        self.money-=money
+        return self.money
--- models/Player.py
+++ models/Player.py
@@ -3,27 +3,73 @@
 from .Money import Money
 from .Hand import Hand
 
-class Player:
-    def __init__(self, id: int = 1, initial_bet: Money = Money(10000)) -> None:
-        self.stakes: Money = initial_bet
+class Player:     
+    def __init__(self,money=Money(500000))->None:
+        self.money: Money =money
         self.hands: [Hand] = []
-        self.id: int = id
+        self.alive:bool=True
+        self.callfee:int=0 #콜비용
+        self.betsum:int=0 #배팅한 돈의 총액
+        self.betfee:int=0 #자신의 턴에 배팅 할 돈
         
-    def bet(self, money: Money) -> None:
-        self.stakes.substitute(money)
+    def setturn(self,turn:int)->None:
+        self.turn:int=turn
+        
+    def getturn(self)->int:
+        return self.turn
     
-    def fold(self) -> None:
-        pass
+    def survive(self)->bool: #getalive()
+        return self.alive   #이걸 여기서 처리할까요?
     
-    def game_in(self) -> None:
-        pass
+    def getbetsum(self)->int:
+        return self.betsum
     
-    def get_hands(self) -> []:
+    def getbetfee(self)->int:
+        return self.betfee
+    
+    def get_hands(self) -> list:
         return self.hands
-
-    def set_hand(self, hands: [Hand]) -> None:
-        self.hands.clear()
-        self.hands.extend(hands)
-
-    def get_id(self) -> int:
-        return self.id
+    
+    def showcallbetfee(self,otherplayerbetsum:int)->int:
+        return abs(self.betsum-otherplayerbetsum)
+    
+    def showhalfbetfee(self,otherplayerbet:int,moneyonthegame:int)->int:
+        return abs(self.betsum-otherplayerbet)+(abs(self.betsum-otherplayerbet)+moneyonthegame)//2
+    
+    def bet(self,money):
+        self.money.minus(money)
+        self.betsum+=money
+    # def die(self):
+    #     self.alive=False
+        
+    def call(self,otherplayerbetsum:int)->None:
+        self.callfee=abs(self.betsum-otherplayerbetsum)
+        self.betfee=self.callfee
+        if self.betfee>self.money.getmoney():
+            self.alive=False
+        self.betsum+=self.betfee
+        self.money.minus(self.betfee)
+        
+    def half(self,otherplayerbet:int,moneyonthegame:int)->None:
+        self.callfee=abs(self.betsum-otherplayerbet)
+        self.betfee=self.callfee+(self.callfee+moneyonthegame)//2
+        if self.betfee>self.money.getmoney():
+            self.alive=False
+        self.betsum+=self.betfee
+        self.money.minus(self.betfee)
+    
+    # def quarter(self,otherplayerbet,moneyonthegame):
+    #     self.callfee=abs(self.betsum-otherplayerbet)
+    #     self.betfee=self.callfee+(self.callfee+moneyonthegame)/4
+    #     if self.betfee>self.money:
+    #         self.alive=False
+    #     self.betsum+=self.betfee
+    #     self.money-=self.betfee
+        
+    # def ddadang(self,otherplayerbet,otherplayercallfee):
+    #     self.callfee=abs(self.betsum-otherplayerbet)
+    #     self.betfee=self.callfee+otherplayercallfee
+    #     if self.betfee>self.money:
+    #         self.alive=False
+    #     self.betsum+=self.betfee
+    #     self.money-=self.betfee
