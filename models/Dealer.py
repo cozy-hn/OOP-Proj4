02c01fcf61c02bc6e8d31528bb4a49e2ffdc28e9
hojooneum <bldolphin96@gmail.com>
2022-12-10 14:14:34 +0900

    Merge Dealer

--- models/Dealer.py
+++ models/Dealer.py
@@ -6,7 +6,7 @@ from services.Round import Round
 import itertools
 import random
 import Rule
-import numpy as np 
+import numpy as np
 
 class Dealer:
     def __init__(self, deck: [Hand], table):
@@ -32,13 +32,11 @@ class Dealer:
                 0.08421,0.08421, 0.08421, 0.08421,
                 0.10,0.10526,0.0578,0.06316,0.07368,
                 0.0738400000000002]
-        
+
         cards= np.random.choice(paeList, 2, p=probabilities)
 
-        player1.hands.clear()
-        player2.hands.clear()
-        player1.hands.extend(cards[0])
-        player2.hands.extend(cards[1])
+        player1.set_hand(cards[0])
+        player2.set_hand(cards[1])
 
 
     def check_game_ended(self, player: Player, computer_player: Player) -> bool:
@@ -49,41 +47,6 @@ class Dealer:
             is_game_ended = True
         return is_game_ended
 
-#def calc_rules(self,paes):
-#    jokbo = Rule.Jokbo.create_jokbo()
-#    paes = sorted(paes, key=lambda x:x)
-#    paes_str = str(paes[0]) + ',' + str(paes[1]#
-
-#    b = jokbo.get(paes_str#
-
-#    if b==None:
-#        tmp_paes = paes[0] % 10
-#        tmp_paes = paes[1] % 10
-#        if tmp_paes == 0:
-#            tmp_paes +=10
-#        if tmp_paes == 0:
-#            tmp_paes +=1#
-
-#        #작은걸 앞으로 해준다. 해서 족보 다 일치시키기
-#        if tmp_paes > tmp_paes:
-#            temp = tmp_paes
-#            tmp_paes = tmp_paes
-#            tmp_paes = tem#
-
-#        paes_str = str(tmp_paes) + ',' + str(tmp_paes) + ',#
-
-#        jokbo_power = jokbo.get(paes_str#
-
-#        if jokbo_power==None:
-#            tmp_inp3 = tmp_paes + tmp_paes
-#            tmp_inp3 %= 1#
-
-#            inp3_str = str(tmp_inp3) + ',,'
-#            jokbo_power = jokbo.get(inp3_str#
-
-#    return jokbo_power
-
-
 if __name__ == '__main__':
     player1 = Player()
     player2 = Player()
@@ -91,7 +54,3 @@ if __name__ == '__main__':
     Dealer.distribute_cards(player1,player2)
     print(player1.get_hands())
     print(player2.get_hands())
-
-
-## test python 3. list_a = list(map(int, list_a))
-
