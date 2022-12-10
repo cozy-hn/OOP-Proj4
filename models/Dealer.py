b466c60475aa57f3a4308d6e95a6149ec65def24
hojooneum <bldolphin96@gmail.com>
2022-12-10 15:46:36 +0900

    Rollback Dealer

--- models/Dealer.py
+++ models/Dealer.py
@@ -2,11 +2,9 @@
 from .Hand import Hand
 from .Player import Player
 from .Hand import Hand
-from services.Round import Round
 import itertools
 import random
 import Rule
-import numpy as np
 
 class Dealer:
     def __init__(self, deck: [Hand], table):
@@ -25,19 +23,13 @@ class Dealer:
     def distribute_cards(self, player1: Player, player2: Player) -> None:
         # 플레이어에게 카드를 분배합니다.
         # 퍼블릭 메소드를 이용해서 플레이어 내부의 멤버변 수 hands에 새로운 패를 넣어줍니다.
-        paeList = [i for i in range(1,29)]
-        # Choose elements with different probabilities
-        probabilities = [0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,0.00526,
-                0.02105,0.02105,0.02105,0.02105,0.02105,0.02105,
-                0.08421,0.08421, 0.08421, 0.08421,
-                0.10,0.10526,0.0578,0.06316,0.07368,
-                0.0738400000000002]
+        temp_player_pae = list()
 
-        cards= np.random.choice(paeList, 2, p=probabilities)
+        temp = list(itertools.combinations(range(1, 21), 2))
+        temp_card_combination = random.sample(sorted(temp), 2)
 
-        player1.set_hand(cards[0])
-        player2.set_hand(cards[1])
-
+        player1.set_hand(list(map(int, temp_card_combination[0])))
+        player2.set_hand(list(map(int, temp_card_combination[1])))
 
     def check_game_ended(self, player: Player, computer_player: Player) -> bool:
         is_game_ended = False
@@ -47,10 +39,36 @@ class Dealer:
             is_game_ended = True
         return is_game_ended
 
-if __name__ == '__main__':
-    player1 = Player()
-    player2 = Player()
+    def calc_rules(self, paes):
+        jokbo = Rule.Jokbo.create_jokbo()
+        paes = sorted(paes, key=lambda x: x)
+        paes_str = str(paes[0]) + ',' + str(paes[1])
 
-    Dealer.distribute_cards(player1,player2)
-    print(player1.get_hands())
-    print(player2.get_hands())
+        b = jokbo.get(paes_str)
+
+        if b == None:
+            tmp_paes = paes[0] % 10
+            tmp_paes = paes[1] % 10
+            if tmp_paes == 0:
+                tmp_paes += 10
+            if tmp_paes == 0:
+                tmp_paes += 10
+
+            # 작은걸 앞으로 해준다. 해서 족보 다 일치시키기
+            if tmp_paes > tmp_paes:
+                temp = tmp_paes
+                tmp_paes = tmp_paes
+                tmp_paes = temp
+
+            paes_str = str(tmp_paes) + ',' + str(tmp_paes) + ','
+
+            jokbo_power = jokbo.get(paes_str)
+
+            if jokbo_power == None:
+                tmp_inp3 = tmp_paes + tmp_paes
+                tmp_inp3 %= 10
+
+                inp3_str = str(tmp_inp3) + ',,'
+                jokbo_power = jokbo.get(inp3_str)
+
+        return jokbo_power
